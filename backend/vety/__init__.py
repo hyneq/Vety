674cbc84654dc64d916cf075248c41555e4153ac
hyneq <hynekv.svobodny@gmail.com>
2025-11-10 19:12:17 +0100

    Feature: Added basic backend app structure

---
+++ backend/vety/__init__.py
@@ -0,0 +1,3 @@
+"""The Vety app backend
+TODO add more detailed description
+"""
---
+++ backend/vety/__main__.py
@@ -0,0 +1,6 @@
+"""The entry point of the app.
+
+When executed, runs a development server with the backend app
+"""
+
+# TODO Add run dev server
