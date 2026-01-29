403df957924bc8e5601105b2b958237d95cdcd47
hyneq <hynekv.svobodny@gmail.com>
2026-01-29 12:53:13 +0100

    Feature: Implemented game logic

--- backend/vety/models/game.py
+++ backend/vety/models/game.py
@@ -4,40 +4,144 @@ The game logic
 TODO: Add description
 """
 
+from typing import Sequence, Mapping, Set, Optional
+from dataclasses import dataclass
+from collections import deque
+
+class GameException(Exception):
+    """
+    A base class for game-related exceptions.
+    """
+
+
 class Game:
     """
     A game in progress
 
     TODO: Add description
     """
-    pass
+
+    _players: Sequence["Player"]
+    _questions: Sequence["Question"]
+    _sentences: Mapping["Player", "Sentence"]
+    _players_answered: Set["Player"]
+
+    finished: bool = False
+    _current_question_i = 0
+
+    def __init__(self,
+                 players: Sequence["Player"],
+                 questions: Sequence["Question"]):
+
+        self._players = players
+        self._questions = questions
+
+        self._sentences = {player: Sentence(questions) for player in players}
+
+    
+    @property
+    def current_question(self) -> "Question":
+        return self._questions[self._current_question_i]
 
 
+    def _get_sentence_for_player(self, player: "Player") -> "Sentence":
+        return self._sentences[player]
+    
+
+    def answer_current_question(self, player: "Player", element: str):
+        self._get_sentence_for_player(player).answer(self.current_question, element)
+
+
+    def proceed(self):
+        if self.finished:
+            raise GameAlreadyFinishedException()
+
+        if self._current_question_i == len(self._questions) - 1:
+            self.finished = True
+            return True
+
+        self._current_question_i += 1
+        self._rotate_sentences()
+
+        return False
+
+
+    def _rotate_sentences(self):
+        values = deque(self._sentences.values())
+        values.rotate(1)
+        self.sentences = dict(zip(self.sentences.keys(), values))
+
+
+    def get_results(self):
+        if not self.finished:
+            raise GameNotFinishedYetException()
+
+        return self._sentences
+
+
+class GameAlreadyFinishedException(GameException):
+    """
+    The game has already finished
+    """
+
+
+class GameNotFinishedYetException(GameException):
+    """
+    The game has not finished yet
+    """
+
+
+@dataclass
 class Player:
     """
     A player participating in a Game
 
-    TODO: Add description
+    Attributes:
+        name: The nickname of the player
     """
 
-    pass
+    name: str
 
 
-class ClauseDef:
+@dataclass
+class Question:
     """
-    A clause definition
+    A specification of a question when building a Sentence
 
-    TODO: Add description
+    Attributes:
+        name: The name of the question, as used internally in the game
+        description: The question in human-friendly language
     """
 
-    pass
+    name: str
+    description: str
 
 
 class Sentence:
     """
     A single sentence in a game
 
-    TODO: Add description
+    The sentence is determined by a mapping of questions to responses (elements)
     """
 
-    pass
+
+    elements: dict[Question,Optional[str]]
+
+    def __init__(self, questions: Sequence[Question]):
+        self.elements = {question: None for question in questions}
+
+
+    def __str__(self) -> str:
+        return " ".join(value for value in self.elements.values() if value)
+
+
+    def answer(self, question: Question, element: str):
+        """
+        Sets the element as the response to question
+        
+        Arguments:
+            question: the question to answer
+            element: the response to the question
+        """
+
+        self.elements[question] = element
