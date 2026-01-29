4087eaa5b4fb921f63e7234eab224b986dd37a43
hyneq <hynekv.svobodny@gmail.com>
2026-01-29 21:05:40 +0100

    Fixed and updated backend/run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-project_dir="$(dirname "$0")"
-venv="$project_dir/venv"
+set -eu
 
-"$venv/bin/python3" -m vety
+project_dir="$(dirname "$0")"
+venv="$project_dir/.venv"
+
+if [[ -z "${PYTHONPATH:-}" ]]; then
+    export PYTHONPATH="$project_dir"
+else
+    export PYTHONPATH="$project_dir:$PYTHONPATH"
+fi
+
+exec "$venv/bin/python3" -m vety
